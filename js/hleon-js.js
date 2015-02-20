06f205cb4d083e5256b759d2d29cd06932b47b07
Hector Leon <hectorleon.designer@gmail.com>
2015-02-20 10:00:56 +0100

    first commit

---
+++ js/hleon-js.js
@@ -0,0 +1,48 @@
+//Jquery
+$(document).ready(function(){
+
+
+	// Button Burguer animation
+	$("#js-btn--burguer").on('click', function() {
+	  $(this).toggleClass("js-active");
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
